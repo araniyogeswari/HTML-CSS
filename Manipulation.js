39e962d949c17ebd55f95374a63447fe3903cfa5
araniyogeswari <araniyogeswari@gmail.com>
2026-06-12 12:00:04 +0530

    Manipulating

---
+++ Manipulation.js
@@ -0,0 +1,11 @@
+let value =document.getElementsByTagName("h1")[0].innertext;
+console.log(value);
+let pen =document.getElementById("wish");
+console.log(pen);
+let pencil= document.getElementsByClassName("wishes");
+console.log(pencil);
+let cable =document.getElementById("div");
+console.log(cable);
+let water= document.getElementsByClassName("#content");
+console.log(water);
+
